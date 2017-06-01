ce9f2f1f3a2f422dd040fb9a256b3ba58c574afe
Jian Zhou <i@janzhou.org>
2017-05-31 22:59:17 -0400

    args for amazon_movies_count

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ LIBS_PATH= ${SST_ELEMENTS_HOME}/lib/sst-elements-library
 COPS = -g -O0
 LIBS= -fopenmp -lpthread
 
-all: thread_write.exe thread_read.exe movie_count.exe movie_count2.exe amazon_movies_count.exe amazon_movies_count2.exe sampling_test.exe
+all: thread_write.exe thread_read.exe movie_count.exe movie_count2.exe amazon_movies_count.exe sampling_test.exe
 
 %.exe: %.o pcm.o movie.o arielapi.o amazon_movies.o
 	$(CXX) -o $@ $^ -L$(LIBS_PATH) -I$(INCLUDE_PATH)  $(LIBS) $(COPS)
--- amazon_movies_count.c
+++ amazon_movies_count.c
@@ -6,6 +6,22 @@ int main(int argc, char *argv[])
 {
 	pcm_param(argc, argv);
 
+	int option;
+	int sample = PCM_NUM_ROWS;
+	int bank_aware_shuffle = 0;
+	int contention_free_r2t = 0;
+
+	while ((option = getopt(argc, argv,"s:a")) != -1) {
+		switch (option) {
+			case 's' : sample = atoi(optarg);
+				   break;
+			case 'a' : bank_aware_shuffle = 1;
+				   break;
+			case 'c' : contention_free_r2t = 1;
+				   break;
+		}
+	}
+
 	char *buf;
 	buf = calloc(1, PCM_SIZE);
 	if (!buf) {
@@ -25,7 +41,17 @@ int main(int argc, char *argv[])
 		rows[r] = r;
 	}
 
-	pcm_r2t_contention_free(pcm_threads, PCM_NUM_BANKS, rows, PCM_NUM_ROWS, buf);
+	if(bank_aware_shuffle == 0) {
+		pcm_rows_shuffle(rows, PCM_NUM_ROWS);
+	} else {
+		pcm_rows_bank_aware_shuffle(rows, PCM_NUM_ROWS);
+	}
+
+	if(contention_free_r2t == 0) {
+		pcm_r2t_even_split(pcm_threads, PCM_NUM_BANKS, rows, PCM_NUM_ROWS, buf);
+	} else {
+		pcm_r2t_contention_free(pcm_threads, PCM_NUM_BANKS, rows, sample, buf);
+	}
 
 	pcm_threads_map_count_fn(pcm_threads, PCM_NUM_BANKS, amazon_movies_cnt_local);	
 
