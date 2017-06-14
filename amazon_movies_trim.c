142d97b3da675bdfaa00bd0e79a7f372a1c934b6
Raghavendra Rao <raghav3276@gmail.com>
2017-06-14 15:32:36 -0400

    Added thred local quick sort

--- amazon_movies_trim.c
+++ amazon_movies_trim.c
@@ -1,6 +1,8 @@
 #include "pcm.h"
 #include "amazon_movies_trim.h"
 
+//#define AMAZON_MOVIES_TRIM_DEBUG
+
 int amazon_movies_trim_init_mem(char *mem)
 {
 	int i, j;
@@ -58,10 +60,10 @@ int amazon_movies_trim_init_mem(char *mem)
 	free (buff);
 	fclose(fp);
 
-#ifdef AMAZON_MOVIES_DEBUG
+#ifdef AMAZON_MOVIES_TRIM_DEBUG
 	for (j = 0; j < PCM_NUM_ROWS; j++) {
 
-		review = (struct amazon_movie_review *) (mem + j * PCM_ROW_SIZE);
+		review = (struct amazon_movie_review_trim *) (mem + j * PCM_ROW_SIZE);
 
 		for (i = 0; i < n_reviews_per_row; i++) {
 
@@ -78,4 +80,55 @@ int amazon_movies_trim_init_mem(char *mem)
 #endif
 
 	return 0;
+}
+
+/* Sorting application */
+void amazon_movies_trim_swap(
+		struct amazon_movie_review_trim *review1,
+		struct amazon_movie_review_trim *review2)
+{
+	struct amazon_movie_review_trim temp_review;
+
+	memset(&temp_review, 0, sizeof(temp_review));
+
+	memcpy(&temp_review, review1, sizeof(*review1));
+	memcpy(review1, review2, sizeof(*review1));
+	memcpy(review2, &temp_review, sizeof(*review1));
+}
+
+int partition(
+		struct amazon_movie_review_trim *review, int l, int h)
+{
+	int i, j;
+	unsigned long pivot = review[h].time;
+
+	for (j = l; j <= h - 1; j++) {
+		if (review[j].time <= pivot) {
+			i++;
+			amazon_movies_trim_swap(&review[i], &review[j]);
+		}
+	}
+
+	amazon_movies_trim_swap(&review[i], &review[j]);
+	return i + 1;
+}
+
+void amazon_movies_trim_quick_sort(
+		struct amazon_movie_review_trim *review, int l, int r)
+{
+	int j;
+
+	if (l < r) {
+		j = partition(review, l, r);
+		amazon_movies_trim_quick_sort(review, l, j - 1);
+		amazon_movies_trim_quick_sort(review, j + 1, r);
+	}
+}
+
+void amazon_movies_trim_sort_local(void *row)
+{
+	struct amazon_movie_review_trim *review;
+	int n = PCM_ROW_SIZE / sizeof(*review);
+
+	amazon_movies_trim_quick_sort(review, 0, n - 1);
 }
